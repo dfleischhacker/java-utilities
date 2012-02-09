a221b0f927677d644dd93b3ee9a798c9a302f307
Daniel Fleischhacker <daniel@informatik.uni-mannheim.de>
2012-02-09 16:31:10 +0100

    Write confidence values separated by axiom types in addition to aggregated values

--- owl-confidence-histogram/src/main/java/de/krkm/utilities/owlconfidencehistogram/ConfidenceValueExtractor.java
+++ owl-confidence-histogram/src/main/java/de/krkm/utilities/owlconfidencehistogram/ConfidenceValueExtractor.java
@@ -16,17 +16,17 @@ public class ConfidenceValueExtractor {
     private List<Double> aggregatedConfidenceList;
 
     /**
-     * Initialize the internal list of annotated axioms contained in the ontology
-     * given by <code>in</code>. Only axioms having one of the annotation properties
-     * contained in <code>properties</code> are considered. The confidence value is
-     * taken from the first property in this list which exists for the specific
-     * axiom.
+     * Initialize the internal list of annotated axioms contained in the ontology given by <code>in</code>. Only axioms
+     * having one of the annotation properties contained in <code>properties</code> are considered. The confidence 
+     * value
+     * is taken from the first property in this list which exists for the specific axiom.
      *
-     * @param in stream containing the ontology to read
+     * @param in         stream containing the ontology to read
      * @param properties annotation properties which contain confidence values
      * @throws OWLOntologyCreationException error loading the ontology
      */
-    public ConfidenceValueExtractor(InputStream in, ArrayList<OWLAnnotationProperty> properties) throws OWLOntologyCreationException {
+    public ConfidenceValueExtractor(InputStream in, ArrayList<OWLAnnotationProperty> properties)
+            throws OWLOntologyCreationException {
         OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
         OWLOntology ontology = manager.loadOntologyFromOntologyDocument(in);
         AnnotatedAxiomExtractor aae = new AnnotatedAxiomExtractor(properties);
@@ -45,7 +45,7 @@ public class ConfidenceValueExtractor {
             if (!confidenceLists.containsKey(axiomType)) {
                 confidenceLists.put(axiomType, new LinkedList<Double>());
             }
-            
+
             confidenceLists.get(axiomType).add(conf);
             aggregatedConfidenceList.add(conf);
         }
@@ -53,7 +53,7 @@ public class ConfidenceValueExtractor {
 
     /**
      * Returns an immutable list of all confidence values contained in the ontology
-     * 
+     *
      * @return immutable list of all confidence values contained in the ontology
      */
     public List<Double> getConfidenceValues() {
@@ -62,7 +62,7 @@ public class ConfidenceValueExtractor {
 
     /**
      * Returns an immutable list of all confidence values for the given OWLAPI AxiomType <code>type</code>.
-     * 
+     *
      * @param type axiom type to return values for
      * @return immutable list of confidence values for given axiom type
      */
@@ -73,4 +73,14 @@ public class ConfidenceValueExtractor {
 
         return Collections.unmodifiableList(confidenceLists.get(type));
     }
+
+    /**
+     * Return all axiom types which are contained in the ontology and annotated with confidence values.
+     *
+     * @return immutable set of all axiom types used in the ontology in conjunction with confidence values 
+     */
+    public Set<AxiomType<?>> getUsedAxiomTypes() {
+        return Collections.unmodifiableSet(confidenceLists.keySet());
+    }
+    
 }
--- owl-confidence-histogram/src/main/java/de/krkm/utilities/owlconfidencehistogram/Main.java
+++ owl-confidence-histogram/src/main/java/de/krkm/utilities/owlconfidencehistogram/Main.java
@@ -3,6 +3,7 @@ package de.krkm.utilities.owlconfidencehistogram;
 import de.krkm.utilities.annotatedaxiomextractor.AnnotatedAxiomExtractor;
 import org.apache.commons.cli.*;
 import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.AxiomType;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 
@@ -19,9 +20,10 @@ public class Main {
         options.addOption(OptionBuilder.withLongOpt("ontology").hasArg().isRequired()
                                        .withArgName("FILENAME")
                                        .withDescription("file to read ontology from").create("o"));
-        options.addOption(OptionBuilder.withLongOpt("output").hasArg().isRequired()
-                                       .withArgName("FILENAME")
-                                       .withDescription("file to write confidence values to").create("out"));
+        options.addOption(OptionBuilder.withLongOpt("out").hasArg().isRequired()
+                                       .withArgName("DIRECTORY")
+                                       .withDescription("directory to write confidence value files to").create
+                        ("out"));
 
 
         CommandLineParser parser = new PosixParser();
@@ -41,7 +43,7 @@ public class Main {
         }
 
         String inFileName = line.getOptionValue("o");
-        String outFileName = line.getOptionValue("out");
+        String outDirName = line.getOptionValue("out");
 
         FileInputStream in = null;
         try {
@@ -52,17 +54,41 @@ public class Main {
             System.exit(2);
         }
 
+        File outDir = new File(outDirName);
+
+        if (!outDir.exists()) {
+            if (!outDir.mkdirs()) {
+                System.err.println("Unable to create non-existing directory " + outDir.getAbsolutePath());
+                System.exit(1);
+            }
+        }
+
         try {
             ConfidenceValueExtractor cve = new ConfidenceValueExtractor(in, AnnotatedAxiomExtractor
                     .getAnnotationsProperties(
                             OWLManager.getOWLDataFactory(), iris.toArray(new IRI[iris.size()])));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(outFileName));
+
+            BufferedWriter writer = new BufferedWriter(
+                    new FileWriter(outDir.getAbsolutePath() + File.separator + "aggregated.txt"));
 
             for (Double val : cve.getConfidenceValues()) {
                 writer.write(String.valueOf(val));
                 writer.newLine();
             }
             writer.close();
+
+            for (AxiomType<?> type : cve.getUsedAxiomTypes()) {
+                writer = new BufferedWriter(
+                        new FileWriter(outDir.getAbsolutePath() + File.separator + filterAxiomTypeName(type) + "" +
+                                               ".txt"));
+
+                for (Double val : cve.getConfidenceValues()) {
+                    writer.write(String.valueOf(val));
+                    writer.newLine();
+                }
+                writer.close();
+            }
+
         }
         catch (OWLOntologyCreationException e) {
             System.err.println("Unable to load ontology: " + e.getMessage());
@@ -78,5 +104,15 @@ public class Main {
 
             }
         }
+    }
+
+    /**
+     * Filter the AxiomType names for usage in filenames
+     *
+     * @param axiomType the axiomtype to get a file name for
+     * @return filtered name of axiom type
+     */
+    public static String filterAxiomTypeName(AxiomType<?> axiomType) {
+        return axiomType.getName().replaceAll("[^A-Za-z0-9]+", "_");
     }
 }
